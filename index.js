04e1b0d987d531f41f8c9e638c0f1ef3d6b42198
cch5ng <cch5ng@gmail.com>
2016-03-01 13:12:31 -0800

    feat: add changes up through end of lesson 5

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
 import React from 'react'
 import { render } from 'react-dom'
+import {Router, Route, useRouterHistory} from 'react-router'
 import App from './modules/App'
-render(<App/>, document.getElementById('app'))
+import About from './modules/About'
+import Repos from './modules/Repos'
+import { createHashHistory } from 'history'
+
+//https://github.com/reactjs/react-router/blob/master/upgrade-guides/v2.0.0.md#using-custom-histories
+//had to apply custom history for 2.0.0 to resolve history error message (had to explicitly define Router history)
+//instead of just using hashHistory from react-router
+
+//useRouterHistory creates a composable higher-order function
+const appHistory = useRouterHistory(createHashHistory)({ queryKey: false })
+render((
+	<Router history={appHistory}>
+		<Route path='/' component={App}>
+			<Route path='/about' component={About} />
+			<Route path='/repos' component={Repos} />
+		</Route>
+	</Router>), document.getElementById('app'))
--- modules/App.js
+++ modules/App.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import {Link} from 'react-router'
 
 export default React.createClass({
-  render() {
-    return <div>Hello, React Router!</div>
-  }
+	render() {
+		return (
+			<div>
+			<h1>React Router Tutorial</h1>
+			<ul role='nav'>
+				<li><Link to='/about' activeClassName='active' >About</Link></li>
+				<li><Link to='/repos' activeClassName='active' >Repos</Link></li>
+			</ul>
+{/* ask about what this is doing */}
+			{this.props.children}
+			</div>
+		)
+	}
 })
-
