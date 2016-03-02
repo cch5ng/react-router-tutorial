a67081bf29d992c22fa69f7e3cb1138542b570fa
cch5ng <cch5ng@gmail.com>
2016-03-02 13:18:38 -0800

    feat: update thru end of lesson 10

--- index.js
+++ index.js
@@ -1,21 +1,14 @@
 import React from 'react'
 import { render } from 'react-dom'
-import {Router, Route, useRouterHistory, IndexRoute} from 'react-router'
+import {Router, Route, IndexRoute, browserHistory} from 'react-router'
 import App from './modules/App'
 import About from './modules/About'
 import Repos from './modules/Repos'
 import Repo from './modules/Repo'
 import Home from './modules/Home'
-import { createHashHistory } from 'history'
 
-//https://github.com/reactjs/react-router/blob/master/upgrade-guides/v2.0.0.md#using-custom-histories
-//had to apply custom history for 2.0.0 to resolve history error message (had to explicitly define Router history)
-//instead of just using hashHistory from react-router
-
-//useRouterHistory creates a composable higher-order function
-const appHistory = useRouterHistory(createHashHistory)({ queryKey: false })
 render((
-	<Router history={appHistory}>
+	<Router history={browserHistory}>
 		<Route path='/' component={App}>
 			<IndexRoute component={Home}/>
 			<Route path='/repos' component={Repos} >
