61a1a328ded0f1e39eb142e22214d3ddcb9bc739
cch5ng <cch5ng@gmail.com>
2016-03-02 17:12:31 -0800

    feat: update thru end lesson 11

--- server.js
+++ server.js
@@ -5,13 +5,14 @@ var path = require('path')
 var compression = require('compression')
 
 var app = express()
+app.use(compression())
 
 // serve our static stuff like index.css
-app.use(express.static(__dirname))
+app.use(express.static(path.join(__dirname, 'public')))
 
 // send all requests to index.html so browserHistory in React Router works
 app.get('*', (req, res) => {
-	res.sendFile(path.join(__dirname, 'index.html'))
+	res.sendFile(path.join(__dirname, 'public', 'index.html'))
 })
 
 var PORT = process.env.PORT || 8080
