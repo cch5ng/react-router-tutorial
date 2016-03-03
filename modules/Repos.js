6cfec7091a34c9f7734318883c2c62db838e0255
cch5ng <cch5ng@gmail.com>
2016-03-03 09:34:52 -0800

    feat: update thru end lesson 12

--- modules/Repos.js
+++ modules/Repos.js
@@ -1,20 +1,41 @@
 // modules/Repos.js
 import React from 'react'
-import {Link} from 'react-router'
+import {Link, browserHistory} from 'react-router'
 import NavLink from './NavLink.js'
 
 export default React.createClass({
+	handleSubmit(ev) {
+		ev.preventDefault()
+		const userName = ev.target.elements[0].value
+		const repo = ev.target.elements[1].value
+		const path = `/repos/${userName}/${repo}`
+		console.log(path)
+		browserHistory.push(path)
+	},
+
 	render() {
 		return (
 			<div>
 				<h2>Repos</h2>
 				<ul>
-					<li><NavLink to='/repos/rackt/react-router'>React Router</NavLink></li>
-					<li><NavLink to='/repos/facebook/react'>React</NavLink></li>
+					<li>
+						<NavLink to="/repos/rackt/react-router">React Router</NavLink>
+					</li>
+					<li>
+						<NavLink to="/repos/facebook/react">React</NavLink>
+					</li>
+					<li>
+						<form onSubmit={this.handleSubmit}>
+							<input type="text" placeholder="userName" /> / {' '}
+							<input type="text" placeholder="repo" /> {' '}
+							<button type="submit">Go</button>
+						</form>
+					</li>
 				</ul>
 
 				{this.props.children}
 			</div>
 		)
 	}
+
 })
